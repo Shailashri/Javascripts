e426b96ee0d311b688f601718028694b6fcd4e19
¨Shailashri <magadumshaila@gmail.com>
2024-01-04 19:49:24 +0100

    added datatypes and variables

---
+++ Training session/session1/datatypes.js
@@ -0,0 +1,21 @@
+var x = 5; //number
+console.log(x);    //  o/p is 5
+console.log(typeof x);  //  o/p is number
+
+var myStr = "I am a string";    //string
+console.log(myStr);
+
+var t = true;                   //boolean
+console.log(t);
+
+var myValue;                   //undefined
+console.log(myValue);
+
+var myData = null;              //null
+console.log(myData);
+
+var myObject = {key: "value", name: "Shaila"};//object
+console.log(myObject);
+
+var myArray = [100, "txt"];   //array
+console.log(myArray);
---
+++ Training session/session1/variable.js
@@ -0,0 +1,19 @@
+/*
+
+var, let, const
+
+RULES
+--short/descriptive , unique
+--Can contain letters, digits, underscores, and dollar signs. 
+--Must begin with a letter or $ and _ .
+--Case sensitive (Ex: NAME and name are different variables). 
+--Reserved words (like JavaScript keywords) cannot be used as names.
+
+*/
+var myName;//initialization
+var myID;
+var myCity;
+myName = "shailashri"; //declaration
+myID = 10;
+myCity = "Bangalore";
+console.log(myName, myID, myCity);// printing all
